9cd1b9edd88410ef2c7d451a131a3929becb46ca
shoiraTa <shoira.shakirovna@gmail.com>
2021-11-10 19:24:47 +0500

    created game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './style.css';
+
+const newGame = {
+  name: 'Sky Force',
+};
+
+console.log(JSON.stringify(newGame));
+
+async function createGame() {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body:
+      JSON.stringify(newGame),
+
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+createGame();
