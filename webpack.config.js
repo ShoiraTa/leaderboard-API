20150e0100df1d6b572f629c0ff54db02a71ba0b
shoiraTa <shoira.shakirovna@gmail.com>
2021-11-11 10:07:03 +0500

    setup: gh-page

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/webpack-demo/',
+    publicPath: '/leaderboard/',
     clean: true,
   },
   module: {
